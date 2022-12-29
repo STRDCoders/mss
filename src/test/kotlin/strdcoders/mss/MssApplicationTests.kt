0ba49a6a519bfe6e3760411289c594f3e946ccc8
tblecher <tomer.blecher.brolix@gmail.com>
2022-12-29 20:37:09 +0000

    First commit

---
+++ src/test/kotlin/strdcoders/mss/MssApplicationTests.kt
@@ -0,0 +1,13 @@
+package strdcoders.mss
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
