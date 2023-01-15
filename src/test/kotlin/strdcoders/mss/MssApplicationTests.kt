5352add1499aaca10147a7136b3534109d95f719
tblecher <tomer.blecher.brolix@gmail.com>
2023-01-15 18:44:50 +0000

    Add Liquibase support

--- src/test/kotlin/strdcoders/mss/MssApplicationTests.kt
+++ src/test/kotlin/strdcoders/mss/MssApplicationTests.kt
@@ -1,9 +1,9 @@
 package strdcoders.mss
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import strdcoders.mss.util.SpringBootTestcontainersMssTest
 
-@SpringBootTest
+@SpringBootTestcontainersMssTest
 class MssApplicationTests {
 
     @Test
